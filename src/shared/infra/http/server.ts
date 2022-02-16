1bee04c1cdd84534033d273513b890f1bd739832
Douglas Soares <douglasmaismcpe@gmail.com>
2022-02-16 19:43:22 -0300

    Using the Middleare Error

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,14 +1,18 @@
 import "reflect-metadata";
+import "express-async-errors";
 import "../typeorm/connection";
 import "../../container";
 
 import express from "express";
 
+import { ErrorMiddleware } from "./middlewares/ErrorMiddleware";
 import { routes } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use(ErrorMiddleware);
 
 app.listen(5500, () => console.log("Server is running ;)"));
