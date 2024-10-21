1908bc523b8b794318d675b440899960bdcab842
Douglas <douglassoares16.dev@gmail.com>
2024-10-21 20:39:26 -0300

    feat: Create Seeds of Items

---
+++ src/shared/infra/typeorm/seeds/items.ts
@@ -0,0 +1,28 @@
+import { v4 as uuid } from "uuid";
+
+import { Item } from "@modules/points/infra/typeorm/entities/Item";
+
+import createConnection from "../connection";
+
+async function addItems() {
+  const connection = await createConnection();
+
+  const newItems = [
+    { title: "Lâmpadas", image: "lampadas.svg" },
+    { title: "Pilhas e Baterias", image: "baterias.svg" },
+    { title: "Papéis e Papelão", image: "papeis-papelao.svg" },
+    { title: "Resíduos Eletrônicos", image: "eletronicos.svg" },
+    { title: "Resíduos Orgânicos", image: "organicos.svg" },
+    { title: "Óleo de Cozinha", image: "oleo.svg" },
+  ];
+
+  const itemRepository = connection.getRepository(Item);
+
+  const items = itemRepository.create(newItems);
+
+  await itemRepository.save(items);
+
+  await connection.close();
+}
+
+addItems().then(() => console.log("Items Created"));
